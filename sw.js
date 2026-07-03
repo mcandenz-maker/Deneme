57246520429322e6874d2cbd73a300b66fea0f9a
mcandenz-maker <mcandenz@gmail.com>
2026-07-03 05:53:08 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'muhasebe-v1';
+const FILES = ['./muhasebe.html', './manifest.json', './icon.svg', './sw.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./muhasebe.html')))
+  );
+});
